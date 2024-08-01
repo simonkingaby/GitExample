ec68ac9f02f2d8f22225f1e5d45101f4dfde6f0c
simonkingaby <skingaby@gmail.com>
2024-08-01 19:21:53 -0400

    I added some actions and activities to the LeftBrain module

---
+++ RoboticsLab.Body.Maui/RoboticsLab.Head.LeftBrain/RoboticsLab.Head.LeftBrain.py
@@ -0,0 +1,16 @@
+
+def Initialize():
+    print("Initializing Left Brain")
+        
+def Think():
+    print("Left Brain is thinking")
+    
+def LookAround():
+    print("Left Brain is looking around")
+    
+def Read():
+    print("Left Brain is reading")
+   
+def Write():
+    print("Left Brain is writing")
+    
