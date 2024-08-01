46fd23f50a1628432970272f3787096eaa9fc41b
hamiltonbrba <Bryanahamiltonbrown@gmail.com>
2024-08-01 19:37:13 -0400

    simplified

--- RoboticsLab.Body.Maui/RoboticsLab.Body.Legs/RoboticsLab.Body.Legs.py
+++ RoboticsLab.Body.Maui/RoboticsLab.Body.Legs/RoboticsLab.Body.Legs.py
@@ -1,30 +1,15 @@
 # roboticslab/body/legs.py
 def Initialize():
-    """
-    Initializes the legs of the robot.
-    """
     print("Initializing the Legs")
 
 def KneeFlexion():
-    """
-    Simulates the flexion movement at the knee joint.
-    """
     print("Leg is flexing at the knee joint")
 
 def KneeExtension():
-    """
-    Simulates the extension movement at the knee joint.
-    """
     print("Leg is extending at the knee joint")
-    
+
 def AnkleDorsiFlexion():
-    """
-    Simulates the dorsi flexion movement at the ankle joint.
-    """
     print("Leg is dorsi flexing at the knee joint")
 
 def AnklePlantarFlexion():
-    """
-    Simulates the plantar flexion movement at the ankle joint
-    """
     print("Leg is plantar flexing at the ankle joint")
