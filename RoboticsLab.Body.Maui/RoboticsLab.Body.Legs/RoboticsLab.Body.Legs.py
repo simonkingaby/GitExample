916d5b8f5cd6e4250b6d7c94718c7c6171b12855
zenobiajazmin <zenobiajazmin@gmail.com>
2024-08-01 19:48:03 -0400

    Added some actions for a leg to do

--- RoboticsLab.Body.Maui/RoboticsLab.Body.Legs/RoboticsLab.Body.Legs.py
+++ RoboticsLab.Body.Maui/RoboticsLab.Body.Legs/RoboticsLab.Body.Legs.py
@@ -1,1 +1,12 @@
 
+def sideswipe():
+    print("Watch out I am side swipping!")
+    
+
+def run():
+    print("Hurry they are coming, you need to RUN!")
+    
+
+def skip():
+    print("Enjoy the weather skip with me!")
+    
